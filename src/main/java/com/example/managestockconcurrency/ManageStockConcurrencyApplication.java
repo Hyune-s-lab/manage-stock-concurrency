c0bfff02a55a051849cc16470ab843b702789f42
hyune <choi8608@gmail.com>
2022-08-19 14:47:58 +0900

    Initial commit

---
+++ src/main/java/com/example/managestockconcurrency/ManageStockConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.example.managestockconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageStockConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageStockConcurrencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/managestockconcurrency/ManageStockConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managestockconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageStockConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
